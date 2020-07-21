014e79bd2f242ac65d6511774d2e4429c5a56f2e
AmirBlackstack <amir.nisar@bkstack.com>
2020-07-21 14:25:24 +0000

    day 2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,13 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+  	@recipe = Recipe.new
+  end
+ 
 end
