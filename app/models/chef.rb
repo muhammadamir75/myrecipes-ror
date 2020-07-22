7d91c18bae78060a3cffd053d883578cdd384a7e
AmirBlackstack <amir.nisar@bkstack.com>
2020-07-22 08:10:53 +0000

    Implement signup

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,6 +3,9 @@ class Chef < ApplicationRecord
   validates :chefname, presence: true, length: {maximum: 30}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255}, 
-                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensative: false }
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensative: false }
   has_many :recipes
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 5 }
 end
