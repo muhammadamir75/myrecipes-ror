1e6c548ddf6cdf42dd96b7b59892075ab18e9e14
AmirBlackstack <amir.nisar@bkstack.com>
2020-07-22 09:53:44 +0000

    listing and pagination

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,8 @@
 class ChefsController < ApplicationController
 
+  def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
+  end
   def new
     @chef = Chef.new
   end
@@ -15,7 +18,22 @@ class ChefsController < ApplicationController
   end
 
   def show
+    @chef = Chef.find(params[:id])
+    @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
+  end
 
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Yoyur account was updated successfully!"
+      redirect_to @chef
+    else
+      render "edit"
+    end
   end
 
   private
